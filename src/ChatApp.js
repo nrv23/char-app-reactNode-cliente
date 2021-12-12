d0e64566e502c18e5e2182d57a7e4cfdd2465e09
nrv23 <navemen23@hotmail.com>
2021-12-12 02:14:07 -0600

    primer commit

---
+++ src/ChatApp.js
@@ -0,0 +1,7 @@
+import { AppRouter } from "./router/AppRouter";
+
+function ChatApp() {
+  return <AppRouter />;
+}
+
+export default ChatApp;
---
+++ src/pages/ChatPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {} from "react-router-dom";
+export const ChatPage = () => {
+  return <div>CHat page</div>
+};
